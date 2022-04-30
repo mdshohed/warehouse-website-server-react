502842ecf80b1ee94a695d10de55989b102be963
mdshohed <mdshohed170@gmail.com>
2022-04-30 12:43:32 +0600

    single product find update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express(); 
 require('dotenv').config()
 const port = process.env.PORT || 5000; 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware 
 app.use(cors());
@@ -34,6 +34,15 @@ async function run() {
       const result = await productCollection.insertOne(newProduct); 
       res.send(result); 
     })
+    // find one product 
+    app.get('/products/:id', async(req, res)=>{
+      const id = req.params.id; 
+      const query = {_id: ObjectId(id)};
+      const product = await productCollection.findOne(query); 
+      res.send(product); 
+    })
+
+
   }
   finally{
 
